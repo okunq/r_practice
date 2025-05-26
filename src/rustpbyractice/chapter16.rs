8c3d9dcb2a67e7302fcfa7b31ff0220092d32550
okunq <kaptuhandrej@gmail.com>
2025-05-27 02:48:49 +0300

    formatted output

--- src/rustpbyractice/chapter16.rs
+++ src/rustpbyractice/chapter16.rs
@@ -1,5 +1,9 @@
-/// https://practice.course.rs/formatted-output/intro.html
-#[test]
-fn test161() {
-
+fn main() {
+   /* Fill in the blanks to make it print:
+   Hello world, I am
+   Sunface!
+   */
+   print!("hello world, ");
+   println!("I am");
+   println!("Sunface!");
 }
