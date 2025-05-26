494fca372456d17fe6ca24445840fe04f235c0d7
okunq <kaptuhandrej@gmail.com>
2025-05-27 01:06:45 +0300

    staircase

---
+++ src/hackerrank/task06.rs
@@ -0,0 +1,26 @@
+use std::io::{self, BufRead};
+
+/*
+ * Complete the 'staircase' function below.
+ *
+ * The function accepts INTEGER n as parameter.
+ */
+
+fn staircase(n: i32) {
+ for i in 1..=n {
+        let spaces = (n - i) as usize;
+        let hashes = i as usize;
+
+        let line = " ".repeat(spaces) + &"#".repeat(hashes);
+        println!("{}", line);
+    }
+}
+
+fn main() {
+    let stdin = io::stdin();
+    let mut stdin_iterator = stdin.lock().lines();
+
+    let n = stdin_iterator.next().unwrap().unwrap().trim().parse::<i32>().unwrap();
+
+    staircase(n);
+}
