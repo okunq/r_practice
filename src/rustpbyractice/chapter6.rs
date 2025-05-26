63e736f73654b3d37144f3602c12d7cf4f7df151
okunq <kaptuhandrej@gmail.com>
2025-05-26 22:32:52 +0300

    array

--- src/rustpbyractice/chapter6.rs
+++ src/rustpbyractice/chapter6.rs
@@ -1,5 +1,9 @@
-/// https://practice.course.rs/compound-types/intro.html
-#[test]
-fn test61() {
+fn main() {
+    // Fill the blank
+    let list: [i32; 100] = [1; 100];
 
+    assert!(list[0] == 1);
+    assert!(list.len() == 100);
+
+    println!("Success!");
 }
