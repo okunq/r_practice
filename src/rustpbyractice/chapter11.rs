b68fa223afe05769a1462d417e7a0e479ade29f3
okunq <kaptuhandrej@gmail.com>
2025-05-27 01:54:45 +0300

    collection types

--- src/rustpbyractice/chapter11.rs
+++ src/rustpbyractice/chapter11.rs
@@ -1,5 +1,13 @@
-/// https://practice.course.rs/collections/intro.html
-#[test]
-fn test111() {
+// FILL in the blank
+fn main() {
+    let mut v1 = Vec::from([1, 2, 4]);
+    v1.pop();
+    v1.push(3);
 
+    let mut v2 = Vec::new();
+    v2.extend_from_slice(&[1, 2, 3]);
+
+    assert_eq!(v1, v2);
+
+    println!("Success!");
 }
