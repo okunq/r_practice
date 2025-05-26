19f671481df0250f851ca6d905ae5d14cf8dd704
okunq <kaptuhandrej@gmail.com>
2025-05-27 01:42:25 +0300

    birthday cake candles

---
+++ src/hackerrank/task08.rs
@@ -0,0 +1,34 @@
+use std::env;
+use std::fs::File;
+use std::io::{self, BufRead, Write};
+
+/*
+ * Complete the 'birthdayCakeCandles' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY candles as parameter.
+ */
+
+fn birthdayCakeCandles(candles: &[i32]) -> i32 {
+let max_height = candles.iter().max().unwrap();
+    candles.iter().filter(|&&c| c == *max_height).count() as i32
+}
+
+fn main() {
+    let stdin = io::stdin();
+    let mut stdin_iterator = stdin.lock().lines();
+
+    let mut fptr = File::create(env::var("OUTPUT_PATH").unwrap()).unwrap();
+
+    let _candles_count = stdin_iterator.next().unwrap().unwrap().trim().parse::<i32>().unwrap();
+
+    let candles: Vec<i32> = stdin_iterator.next().unwrap().unwrap()
+        .trim_end()
+        .split(' ')
+        .map(|s| s.to_string().parse::<i32>().unwrap())
+        .collect();
+
+    let result = birthdayCakeCandles(&candles);
+
+    writeln!(&mut fptr, "{}", result).ok();
+}
