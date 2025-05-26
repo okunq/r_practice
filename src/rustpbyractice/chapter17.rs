b1ad8488828d9beeb69b67925ba1f3691117286d
okunq <kaptuhandrej@gmail.com>
2025-05-27 02:49:57 +0300

    lifetime

--- src/rustpbyractice/chapter17.rs
+++ src/rustpbyractice/chapter17.rs
@@ -1,5 +1,13 @@
-/// https://practice.course.rs/lifetime/intro.html
-#[test]
-fn test171() {
+fn main() {
+    let static_string = "I'm in read-only memory";
+    {
+        // Make a `string` literal and print it:
 
+        println!("static_string: {}", static_string);
+
+        // When `static_string` goes out of scope, the reference
+        // can no longer be used, but the data remains in the binary.
+    }
+
+    println!("static_string reference remains alive: {}", static_string);
 }
