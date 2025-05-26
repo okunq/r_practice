7e782ef58583db4ddf03b0988770f76a9a4daa52
okunq <kaptuhandrej@gmail.com>
2025-05-27 02:53:45 +0300

    newtype and dst

--- src/rustpbyractice/chapter19.rs
+++ src/rustpbyractice/chapter19.rs
@@ -1,5 +1,26 @@
-/// https://practice.course.rs/newtype-sized.html
-#[test]
-fn test191() {
+use std::ops::Add;
+use std::fmt::{self, format};
 
+struct Meters(u32);
+impl fmt::Display for Meters {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "There are still {} meters left", self.0)
+    }
+}
+
+impl Add for Meters {
+    type Output = Self;
+
+    fn add(self, other: Meters) -> Self {
+        Self(self.0 + other.0)
+    }
+}
+/* Implement calculate_distance  */
+fn calculate_distance(a: Meters, b: Meters) -> Meters {
+    a + b
+}
+
+fn main() {
+    let d = calculate_distance(Meters(10), Meters(20));
+    assert_eq!(format!("{}",d), "There are still 30 meters left");
 }
