f90c545874806703bb35cdb60ba9932783be48f5
okunq <kaptuhandrej@gmail.com>
2025-05-27 01:29:49 +0300

    method&associated function

--- src/rustpbyractice/chapter9.rs
+++ src/rustpbyractice/chapter9.rs
@@ -1,5 +1,22 @@
-/// https://practice.course.rs/method.html
-#[test]
-fn test91() {
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
 
+// Using multiple `impl` blocks to rewrite the code below.
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+}
+    impl Rectangle {
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
+
+
+fn main() {
+    println!("Success!");
 }
